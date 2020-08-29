7187642e017f116416c064bf9ebc2ed5c86d1f00
Evgeny Ebergard <eugeneebergard@gmail.com>
2020-08-30 00:31:15 +0500

    try to fix cors 6

--- app.js
+++ app.js
@@ -37,7 +37,13 @@ mongoose.connect(url, {
 
 app.use(requestLogger);
 
-app.post('/signup',
+app.post('/signup', function(req, res) {
+  res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.set('Access-Control-Allow-Headers', 'Content-Type')
+  res.send(JSON.stringify({
+    message: 'This is public info'
+  }),
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
